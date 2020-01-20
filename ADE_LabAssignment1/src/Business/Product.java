8f0be7590bdcd9e3bae2e15ed54513a485bcbec3
SandhyaGitH <goswami.sa@husky.neu.edu>
2020-01-20 23:11:09 +0530

    ADE Lab Assignment1 Initial CheckIn

---
+++ ADE_LabAssignment1/src/Business/Product.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Business;
+
+/**
+ *
+ * @author samarth
+ */
+public class Product {
+    private String name;
+    private String price;
+    private String availability;
+    private String Description;
+    //private String phoneNumber;
+
+    public String getDescription() {
+        return Description;
+    }
+
+    public void setDescription(String Description) {
+        this.Description = Description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public String getAvailability() {
+        return availability;
+    }
+
+    public void setAvailability(String availability) {
+        this.availability = availability;
+    }
+}
